aeab30a3d94e7a65f4ddaaacf3cb355207b073ea
pankajshetake07 <140935206+pankajshetake07@users.noreply.github.com>
2025-01-05 12:52:00 +0530

    initial commit

---
+++ src/components/DoctorHome.js
@@ -0,0 +1,7 @@
+export default function DoctorHome() {
+    return (
+        <div>
+            <h1>This is Doctor home Page</h1>
+        </div>
+    )
+}
---
+++ src/components/PatientHome.js
@@ -0,0 +1,9 @@
+export default function PatientHome() {
+    return (
+        <div>
+            <h1>
+                This is Patient home Page
+            </h1>
+        </div>
+    )
+}
