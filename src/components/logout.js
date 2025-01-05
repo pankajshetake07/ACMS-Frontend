64ff2ad8dacd3356a7a364c42d3d7062fad291bd
pankajshetake07 <140935206+pankajshetake07@users.noreply.github.com>
2025-01-05 13:07:01 +0530

    added comment

--- src/components/logout.js
+++ src/components/logout.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { logout } from "./slice";
 
 export default function Logout() {
+    // this is for logout
     const navigate = useNavigate();
     const dispatch = useDispatch();
     localStorage.clear();
